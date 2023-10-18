115ff2d8bc6c5016d303173ba01f12bb4b904e1b
PascuFCalvo <pascual.fernandez.calvo@gmail.com>
2023-10-18 20:41:14 +0200

    testing express.js

---
+++ src/index.ts
@@ -0,0 +1,67 @@
+import express from "express";
+import * as readline from "readline";
+
+const app = express();
+app.use(express.json());
+
+let users: any = ["Pascual", "Ana", "Pepe", "Dani"];
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("server running, listening on port: ", PORT);
+});
+
+app.get("/status", (req, res) => {
+  const status = { Status: "Running" };
+  return res.send(status);
+});
+
+// Get all users
+app.get("/users", (req, res) => {
+  return res.json(users);
+});
+
+// Add users
+app.post("/users", (req, res) => {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  rl.question("Enter the new user to add to user list: ", (name) => {
+    if (name) {
+      users.push(name);
+      return res.send(`User >>${name}<< added to the list [${users}]`,); // Agrega el nuevo usuario a la matriz
+      rl.close();
+       // Devuelve la lista actualizada de usuarios
+    } else {
+      rl.close();
+      return res.status(400).send("Please, enter a valid name.");
+    }
+  });
+});
+
+// Delete users
+app.delete("/users", (req, res) => {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+  console.log(users);
+  rl.question(
+    `Enter a name of the list to delete[${users}]=>`,
+    (userName) => {
+      rl.close();
+
+      const index = users.indexOf(userName);
+
+      if (index === -1) {
+        return res.status(400).send(`user ${userName} no exists`);
+      }
+
+      users.splice(index, 1);
+      return res.send(`User >>${userName}<< deleted from users list`);
+    }
+  );
+});
