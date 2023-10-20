76f6914eb90c9c94496218382cb161aada26b6de
PascuFCalvo <pascual.fernandez.calvo@gmail.com>
2023-10-20 15:08:36 +0200

    feat: added try-catchs

--- src/controllers/usersControllers.ts
+++ src/controllers/usersControllers.ts
@@ -1,55 +1,41 @@
 import  { Request, Response} from "express";
-import * as readline from "readline";
 
-let users: any = ["Pascual", "Ana", "Pepe", "Dani"];
+
 
 const getAllUsers = (req: Request, res:Response) => {
-   console.log(users)
-   return res.json(users);
+  try {
+    
+  } catch (error) {
+    
+  }
 }
-
-const getUserById = () => {}
+const getUserById = (req: Request, res:Response) => {
+  try {
+    
+  } catch (error) {
+    
+  }
+}
 const createUserById = (req: Request, res:Response) => {
-   const rl = readline.createInterface({
-      input: process.stdin,
-      output: process.stdout,
-    });
-  
-    rl.question("Enter the new user to add to user list: ", (name) => {
-      if (name) {
-        users.push(name); // Agrega el nuevo usuario al array
-        
-        rl.close();
-        return res.send(`User >>${name}<< added to the list [${users}]`); // Devuelve la lista actualizada de usuarios
-      } else {
-        rl.close();
-        return res.status(400).send("Please, enter a valid name.");
-      }
-    });
+  try {
+    
+  } catch (error) {
+    
+  }
 }
 const deleteUserById = (req: Request, res:Response) => {
-   const rl = readline.createInterface({
-      input: process.stdin,
-      output: process.stdout,
-    });
-   
-    rl.question(`Enter a name of the list to delete[${users}]=>`, (userName) => {
-      rl.close();
-  
-      
-  
-      const index = users.indexOf(userName);
-  
-      if (index === -1) {
-        return res.status(400).send(`user ${userName} no exists`);
-      }
-  
-      users.splice(index, 1);
-      
-      return res.send(`User >>${userName}<< deleted from users list [${users}]`);
-      
-    });
+  try {
+    
+  } catch (error) {
+    
+  }
 }
-const modifyUserById = () => {}
+const modifyUserById = () => {
+  try {
+    
+  } catch (error) {
+    
+  }
+}
 
 export { getAllUsers, getUserById, createUserById, deleteUserById, modifyUserById }
